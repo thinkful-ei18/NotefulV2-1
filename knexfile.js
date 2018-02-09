fff31e6d81969e6f2cd8339184668ab4bf9c6b2b
Brian Dudey <dudey.brian@gmail.com>
2018-02-09 14:11:42 -0600

    Second Commit - Ready for Takeoff?

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,14 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://localhost/noteful',
+    connection: process.env.DATABASE_URL || 'postgresql://dev:pass123@localhost/dev-noteful-app',
     debug: true, // http://knexjs.org/#Installation-debug
-    pool: {min : 1 , max : 2}
+    pool: { min: 1, max: 2 }
   },
   test: {
     client: 'pg',
-    connection: process.env.TEST_DATABASE_URL || 'postgres://localhost/noteful-test',
-    pool: {min : 1 , max : 2}
+    connection: process.env.TEST_DATABASE_URL || 'postgres://dev:pass123@localhost/noteful-test',
+    pool: { min: 1, max: 2 }
   },
   production: {
     client: 'pg',
